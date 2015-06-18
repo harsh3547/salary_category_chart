bbdacf323d1a699126b222f356c661a3b267f6dc
Harsh Jain <harsh.jain@jginfosystems.com>
2015-06-18 12:24:23 +0530

    if not period_obj or fiscalyear_id in context then default present periods default state = done payslip_ids === for which the date_to lies between the period from and to salary_category,total from the payslip lines of those payslips adding amount to the categ_id and the same amount to its parents

--- salary_chart.py
+++ salary_chart.py
@@ -7,19 +7,68 @@ class hr_salary_rule_category(osv.osv):
     """
     
     def _sum_period(self,cr,uid,ids, name,args,context):
-        print "===context======",context
+        #print "\n\n===period==context======",context
         res={}
         for id in ids:
             res[id]=0.0
+        #print "-----------res orginal",res
+            
+        present_period_id = self.pool.get('account.period').find(cr, uid, context=context)
+        period_id = context.get('period_obj',False) or (present_period_id and present_period_id[0]) or False
+        state = context.get('state',False) or 'done' 
+        if period_id and state:
+            #print "=========period_id,state==",period_id,state
+            cr.execute("select id from hr_payslip where date_to >= (select date_start from account_period where id = %s) and date_to <= (select date_stop from account_period where id = %s) and state = %s",(period_id,period_id,state,))
+            payslip_ids=tuple([i[0] for i in cr.fetchall()])
+            #print "0-0-0-0-0-payslip_ids=====",payslip_ids
+            if not payslip_ids:return res
+            cr.execute("select category_id,total from hr_payslip_line where slip_id in %s",(payslip_ids,))
+            categ_total=cr.fetchall()
+            #print "-0-0-0-0-0--categ_total-",categ_total
+            for val in categ_total:
+                id=val[0]
+                amount=val[1]
+                parent_id=True
+                while parent_id != None:
+                    res[id] =res[id] + amount if res.get(id,False) else amount
+                    cr.execute("select parent_id from hr_salary_rule_category where id = %s",(id,))
+                    parent_id=cr.fetchall()[0][0]
+                    id=parent_id
+        #print "----------------0-0-0 final value",res
         return res
 
     
     def _sum_year(self,cr,uid,ids, name,args,context):
-        print "===context======",context
+        #print "\n\n\n===context==========year====",context
         res={}
         for id in ids:
             res[id]=0.0
+        #print "-----------res orginal",res
+        
+        fiscalyear_id = self.pool.get('account.fiscalyear').finds(cr, uid, exception=False)
+        presentyear_id = context.get('fiscalyear_id',False) or (fiscalyear_id and fiscalyear_id[0]) or False
+        state = context.get('state',False) or 'done' 
+        if presentyear_id and state:
+            #print "=========presentyear_id,state==",presentyear_id,state
+            cr.execute("select id from hr_payslip where date_to >= (select date_start from account_fiscalyear where id = %s) and date_to <= (select date_stop from account_fiscalyear where id = %s) and state = %s",(presentyear_id,presentyear_id,state,))
+            payslip_ids=tuple([i[0] for i in cr.fetchall()])
+            #print "0-0-0-0-0-payslip_ids=====",payslip_ids
+            if not payslip_ids:return res
+            cr.execute("select category_id,total from hr_payslip_line where slip_id in %s",(payslip_ids,))
+            categ_total=cr.fetchall()
+            #print "-0-0-0-0-0--categ_total-",categ_total
+            for val in categ_total:
+                id=val[0]
+                amount=val[1]
+                parent_id=True
+                while parent_id != None:
+                    res[id] =res[id] + amount if res.get(id,False) else amount
+                    cr.execute("select parent_id from hr_salary_rule_category where id = %s",(id,))
+                    parent_id=cr.fetchall()[0][0]
+                    id=parent_id
+        #print "----------------0-0-0 final value",res
         return res
+
 
     _inherit = 'hr.salary.rule.category'
     _description = 'Salary Rule Category'
